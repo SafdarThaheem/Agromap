962cba5a8bc7fda626367469ad83f4886ff7db82
SafdarThaheem <104439594+SafdarThaheem@users.noreply.github.com>
2022-08-03 12:13:41 +0500

    Navbar setting

--- index.php
+++ index.php
@@ -49,15 +49,19 @@
 
 <body>
     <!-- statr Nav  -->
-    <nav class="navbar navbar-expand-lg navbar-light py-0 custom-nav">
-        <div class="container-fluid px-0">
+    <nav class="navbar navbar-expand-lg py-0 main-nav">
+        <div class="container-fluid inner-div">
+            <a href="#">
+                <img src="images/ico_logo/Menu_Icon feather.svg" class="sidebar-menu-icon" alt="">
+            </a>
             <a class="navbar-brand m-auto py-0" href="#">
-                <img src="images/AgroMap-top-logo.png" alt="" style="width: 260px; height:51px;">
+                <img src="images/AgroMap-top-logo.png" alt="" style="width: 260px;">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                <img src="images/ico_logo/Menu_Icon feather.svg" class="sidebar-menu-icon" alt="">
+                <!-- <span class="navbar-toggler-icon"></span> -->
             </button>
-            <div class="collapse navbar-collapse flex-grow-0" id="navbarTogglerDemo03">
+            <div class="collapse navbar-collapse flex-grow-0 tog-div" id="navbarTogglerDemo03">
                 <ul class="navbar-nav mx-auto sm-icon">
                     <li class="nav-item">
                         <a class="nav-link active" href="#"><img src="images/ico_logo/Search.svg" alt=""></a>
@@ -81,12 +85,12 @@
     <!-- end nav -->
 
     <!-- statr Nav form -->
-    <nav class="navbar navbar-expand-lg navbar-light py-0 custom-form" style="background-color: #2D2D2D;">
+    <nav class="navbar navbar-expand-lg navbar-light py-0" style="background-color: #2D2D2D;">
         <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-            <div id="filterPanel" style="padding: 10px 5px;">
+            <div id="filterPanel" class="form-outer-div">
                 <form style="color: white;">
                     <div class="row">
-                        <div class=" col-md-1 form-group" style="width: 120px;">
+                        <div class=" col-md form-group" >
                             <label for="Tipo" id="Tipo_label">Tipo</label>
                             <select name="Tipo" class="form-control form-select rounded-pill" id="Tipo_select">
                                 <option value="">Tipo</option>
@@ -98,7 +102,7 @@
                             </select>
                         </div>
 
-                        <div class=" col-md-1 form-group" style="width: 160px;">
+                        <div class=" col-md form-group">
                             <label for="District" id="District">Distrito</label>
                             <select name="District" class="form-control form-select rounded-pill" id="District_select">
                                 <option value="">Distrito</option>
@@ -110,42 +114,42 @@
                             </select>
                         </div>
 
-                        <div class=" col-md-2 form-group" style="width: 160px;">
+                        <div class=" col-md form-group">
                             <label>Fecha de siembra</label>
                             <input name="Plant_date_from" type="date" class="form-control rounded-pill" id="Plant_date_from" placeholder="">
                             <label>To</label>
                             <input name="Plant_date_to" type="date" class="form-control rounded-pill" id="Plant_date_to" placeholder="">
                         </div>
 
-                        <div class=" col-md-2 form-group" style="width: 160px;">
+                        <div class=" col-md form-group">
                             <label>Fecha de cosecha</label>
                             <input name="Harvest_date_from" type="date" class="form-control rounded-pill" id="Harvest_date_from">
                             <label>To</label>
                             <input name="Harvest_date_to" type="date" class="form-control rounded-pill" id="Harvest_date_to">
                         </div>
 
-                        <div class=" col-md-2 form-group" style="width: 190px;">
+                        <div class=" col-md form-group">
                             <label>Fecha de actualización</label>
                             <input name="Update_date_from" type="date" class="form-control rounded-pill" id="Update_date_from">
                             <label>To</label>
                             <input name="Update_date_to" type="date" class="form-control rounded-pill" id="Update_date_to">
                         </div>
 
-                        <div class=" col-md-1 form-group" style="width: 140px;">
+                        <div class=" col-md form-group">
                             <label>Área sembrada</label>
                             <input name="Plant_area_from" type="number" class="form-control rounded-pill" min="0" placeholder="min" id="Plant_area_from">
                             <label>To</label>
                             <input name="Plant_area_to" type="number" class="form-control rounded-pill" min="0" placeholder="max" id="Plant_area_to">
                         </div>
 
-                        <div class=" col-md-1 form-group" style="width: 140px;">
+                        <div class=" col-md form-group">
                             <label>Área cosechada</label>
                             <input name="Harvest_area_from" type="number" class="form-control rounded-pill" min="0" placeholder="min" id="Harvest_area_from">
                             <label>To</label>
                             <input name="Harvest_area_to" type="number" class="form-control rounded-pill" min="0" placeholder="max" id="Harvest_area_to">
                         </div>
 
-                        <div class=" col-md-1 form-group" style="width: 120px;">
+                        <div class=" col-md form-group">
                             <label>Responsable</label>
                             <select name="Responsabl" class="form-control form-select rounded-pill" id="Responsible_select">
                                 <option value="">Responsable</option>
@@ -156,15 +160,12 @@
                                 <option value="">Variedad</option>
                             </select>
                         </div>
-                    </div>
 
-                    <div class="row">
-                        <div class=" col-md-4 form-group" style="float: right; right: 3.5%">
-                            <input name="submit" type="submit" style="float: right; border-radius: 10px; margin: 10px" class="btn btn-primary" value="Aplicar">
-                            <input name="clear" id="clearFilter" type="submit" style="float: right; border-radius: 10px; margin: 10px" class="btn btn-primary" value="Limpiar">
+                        <div class="col-md form-group">
+                            <input name="submit" type="submit" style="border-radius: 10px;" class="btn btn-primary" value="Aplicar">
+                            <input name="clear" id="clearFilter" type="submit" style="border-radius: 10px;" class="btn btn-primary" value="Limpiar">
                         </div>
                     </div>
-
                 </form>
             </div>
         </div>
@@ -182,7 +183,7 @@
         <a href="#" style="margin-left: -15px;"><img src="images/Base_map.svg" alt=""></i></a>
     </div>
 
-    <div id="main" class="container-fluid">
+    <!-- <div id="main" class="container-fluid">
 
         <div class="row">
             <div style="background-color: black;opacity: 0.7;" class="col-md-12">
@@ -214,7 +215,7 @@
         </div>
 
         <br><br>
-    </div>
+    </div> -->
 
 </body>
 
